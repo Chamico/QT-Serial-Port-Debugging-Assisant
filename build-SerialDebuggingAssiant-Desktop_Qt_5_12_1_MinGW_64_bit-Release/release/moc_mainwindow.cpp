f9b6193e5e44e8ee10f0642e5c6b76dcaf791b61
chamico <chamico@aliyun.com>
2019-03-03 15:50:05 +0800

    Finish all the Commit

--- build-SerialDebuggingAssiant-Desktop_Qt_5_12_1_MinGW_64_bit-Release/release/moc_mainwindow.cpp
+++ build-SerialDebuggingAssiant-Desktop_Qt_5_12_1_MinGW_64_bit-Release/release/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../SerialDebuggingAssiant/mainwindow.h"
+#include "../../QT-Serial-Port-Debugging-Assisant/SerialDebuggingAssiant/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[1];
-    char stringdata0[11];
+    QByteArrayData data[24];
+    char stringdata0[364];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,41 @@ struct qt_meta_stringdata_MainWindow_t {
     )
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "MainWindow"
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 13), // "ConnectSignal"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 13), // "WidgetInitial"
+QT_MOC_LITERAL(4, 40, 12), // "SerialPortOn"
+QT_MOC_LITERAL(5, 53, 13), // "SerialPortOff"
+QT_MOC_LITERAL(6, 67, 17), // "SerialPortSetBaud"
+QT_MOC_LITERAL(7, 85, 20), // "SerialPortSetDataBit"
+QT_MOC_LITERAL(8, 106, 25), // "SerialPortSetFlowContronl"
+QT_MOC_LITERAL(9, 132, 19), // "SerialPortSetParity"
+QT_MOC_LITERAL(10, 152, 20), // "SerialPortSetStopBit"
+QT_MOC_LITERAL(11, 173, 15), // "UpdateErrorType"
+QT_MOC_LITERAL(12, 189, 22), // "UpdateSerialPortNumber"
+QT_MOC_LITERAL(13, 212, 18), // "UpdateSendAutomaic"
+QT_MOC_LITERAL(14, 231, 13), // "UpdateReceive"
+QT_MOC_LITERAL(15, 245, 12), // "ReceiveClear"
+QT_MOC_LITERAL(16, 258, 12), // "ReceivePause"
+QT_MOC_LITERAL(17, 271, 18), // "ReceiveTextDisplay"
+QT_MOC_LITERAL(18, 290, 17), // "ReceiveHexDisplay"
+QT_MOC_LITERAL(19, 308, 9), // "SendClear"
+QT_MOC_LITERAL(20, 318, 13), // "SendAutomatic"
+QT_MOC_LITERAL(21, 332, 17), // "SendAutomaticTime"
+QT_MOC_LITERAL(22, 350, 8), // "SendData"
+QT_MOC_LITERAL(23, 359, 4) // "Help"
 
     },
-    "MainWindow"
+    "MainWindow\0ConnectSignal\0\0WidgetInitial\0"
+    "SerialPortOn\0SerialPortOff\0SerialPortSetBaud\0"
+    "SerialPortSetDataBit\0SerialPortSetFlowContronl\0"
+    "SerialPortSetParity\0SerialPortSetStopBit\0"
+    "UpdateErrorType\0UpdateSerialPortNumber\0"
+    "UpdateSendAutomaic\0UpdateReceive\0"
+    "ReceiveClear\0ReceivePause\0ReceiveTextDisplay\0"
+    "ReceiveHexDisplay\0SendClear\0SendAutomatic\0"
+    "SendAutomaticTime\0SendData\0Help"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,21 +75,95 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+      22,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  124,    2, 0x08 /* Private */,
+       3,    0,  125,    2, 0x08 /* Private */,
+       4,    0,  126,    2, 0x08 /* Private */,
+       5,    0,  127,    2, 0x08 /* Private */,
+       6,    0,  128,    2, 0x08 /* Private */,
+       7,    0,  129,    2, 0x08 /* Private */,
+       8,    0,  130,    2, 0x08 /* Private */,
+       9,    0,  131,    2, 0x08 /* Private */,
+      10,    0,  132,    2, 0x08 /* Private */,
+      11,    0,  133,    2, 0x08 /* Private */,
+      12,    0,  134,    2, 0x08 /* Private */,
+      13,    0,  135,    2, 0x08 /* Private */,
+      14,    0,  136,    2, 0x08 /* Private */,
+      15,    0,  137,    2, 0x08 /* Private */,
+      16,    0,  138,    2, 0x08 /* Private */,
+      17,    0,  139,    2, 0x08 /* Private */,
+      18,    0,  140,    2, 0x08 /* Private */,
+      19,    0,  141,    2, 0x08 /* Private */,
+      20,    0,  142,    2, 0x08 /* Private */,
+      21,    0,  143,    2, 0x08 /* Private */,
+      22,    0,  144,    2, 0x08 /* Private */,
+      23,    0,  145,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->ConnectSignal(); break;
+        case 1: _t->WidgetInitial(); break;
+        case 2: _t->SerialPortOn(); break;
+        case 3: _t->SerialPortOff(); break;
+        case 4: _t->SerialPortSetBaud(); break;
+        case 5: _t->SerialPortSetDataBit(); break;
+        case 6: _t->SerialPortSetFlowContronl(); break;
+        case 7: _t->SerialPortSetParity(); break;
+        case 8: _t->SerialPortSetStopBit(); break;
+        case 9: _t->UpdateErrorType(); break;
+        case 10: _t->UpdateSerialPortNumber(); break;
+        case 11: _t->UpdateSendAutomaic(); break;
+        case 12: _t->UpdateReceive(); break;
+        case 13: _t->ReceiveClear(); break;
+        case 14: _t->ReceivePause(); break;
+        case 15: _t->ReceiveTextDisplay(); break;
+        case 16: _t->ReceiveHexDisplay(); break;
+        case 17: _t->SendClear(); break;
+        case 18: _t->SendAutomatic(); break;
+        case 19: _t->SendAutomaticTime(); break;
+        case 20: _t->SendData(); break;
+        case 21: _t->Help(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -88,6 +193,17 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 22)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 22;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 22)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 22;
+    }
     return _id;
 }
 QT_WARNING_POP
